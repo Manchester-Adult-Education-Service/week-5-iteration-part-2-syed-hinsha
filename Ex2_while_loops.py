69dbf418fb385cd2cccd1bc4bfd12ebb0aeb03e6
816231-hins <816231@manadulted.ac.uk>
2025-10-16 12:10:27 +0000

    Completed task 1-counting loop

--- Ex2_while_loops.py
+++ Ex2_while_loops.py
@@ -49,6 +49,10 @@ print("-------------------------------------------\n"
 #     number = number + 1
 #
 # Write your code below:
+count=1
+while count<=5:
+    print(count)
+    count=count+1
 
 
 # -------------------------------------------
