e6bdcd3a22a2ba4a1e4ddd40b1f8fcb77e2202f3
816231-hins <816231@manadulted.ac.uk>
2025-10-16 13:25:06 +0000

    Completed exnsion 3

--- Ex2_while_loops.py
+++ Ex2_while_loops.py
@@ -242,6 +242,19 @@ print("-------------------------------------------\n"
 # Write your code below:
 
 
+
+total=0
+while total <50:
+    number1=int(input("Enter first number :"))
+    number2=int(input("Enter second number:"))
+    total=int(number1+number2)
+    print("Total",total)
+
+
+
+
+
+
 # -------------------------------------------
 # SWAP COMPUTERS
 # -------------------------------------------
